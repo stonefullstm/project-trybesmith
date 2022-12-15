a4d6d6ad1d03eec58d7eb82268cc8c0e48c3a0ee
stonefullstm <stonefull.stm@gmail.com>
2022-12-15 16:55:59 -0300

    Requisito 8 - criar o endpoint POST /orders

--- src/middlewares/validateOrder.ts
+++ src/middlewares/validateOrder.ts
@@ -3,15 +3,16 @@ import Joi from 'joi';
 
 const orderSchema = Joi.object(
   {
-    productsIds: Joi.array().items(Joi.number().required()).required(),
-    user: Joi.object(),
+    productsIds: Joi.array().items(Joi.required()).required(),
+    // user: Joi.object(),
   },
 ).messages({ 
   'array.includesRequiredUnknowns': '"productsIds" must include only numbers',
 });
 
 const validateOrder = (req: Request, res: Response, next: NextFunction) => {
-  const { error } = orderSchema.validate(req.body);
+  const { productsIds } = req.body;
+  const { error } = orderSchema.validate({ productsIds });
   if (error) {
     const [details] = error.details;
     const statusCode = details.type === 'any.required' ? 400 : 422; 
